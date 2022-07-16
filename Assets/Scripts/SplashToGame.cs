1e1be70f9efa5157a7e9672406d53f370fe8ad68
Akrima Huzaifa Akhtar <akrimaamirka@gmail.com>
2022-07-16 09:22:38 +0500

    app completed

---
+++ Assets/Scripts/SplashToGame.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashToGame : MonoBehaviour
+{
+    [Header("BackGrounds")]
+    public GameObject bgm;
+    public GameObject splashImage;
+    public GameObject splashText;
+    
+
+    void Start()
+    {
+        StartCoroutine(SplashEnd());
+    }
+
+    IEnumerator SplashEnd()
+    {
+        yield return new WaitForSeconds(4);
+        bgm.SetActive(true);
+        yield return new WaitForSeconds(1);
+        splashText.SetActive(false);
+        splashImage.SetActive(false);
+    }
+}
